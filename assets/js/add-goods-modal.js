9392d2566d09b262d08aded9f75550a1228b555a
Rostyslavkoo <st1ker7790@gmail.com>
2021-06-21 18:39:55 +0300

    modal_avaible

--- assets/js/add-goods-modal.js
+++ assets/js/add-goods-modal.js
@@ -4,31 +4,35 @@ let addGlass = document.querySelector('.add_glasses')
 let btnCase = document.querySelector('#btnAddCase')
 let btnCharge = document.querySelector('#btnAddCharge')
 let btnGlass = document.querySelector('#btnAddGlass')
+let addGoodsModal = document.querySelector('#addGoodsModal')
 
 
 function show_cases(){
   addCharge.style.display = 'none';
   addGlass.style.display = 'none';
   addCase.style.display = 'flex';
-  btnCase.classList.add("_active-btn-case");
-  btnGlass.classList.remove("_active-btn-glass");
-  btnCharge.classList.remove("_active-btn-charge");
+  btnCase.classList.add("_active-btn");
+  btnGlass.classList.remove("_active-btn");
+  btnCharge.classList.remove("_active-btn");
+
 }
 function show_charge(){
   addCharge.style.display = 'flex';
   addGlass.style.display = 'none';
   addCase.style.display = 'none';
-  btnCharge.classList.add("_active-btn-charge");
-  btnCase.classList.remove("_active-btn-case");
-  btnGlass.classList.remove("_active-btn-glass");
+  btnCharge.classList.add("_active-btn");
+  btnCase.classList.remove("_active-btn");
+  btnGlass.classList.remove("_active-btn");
+
 
 }
 function show_glasse(){
   addCharge.style.display = 'none';
   addGlass.style.display = 'flex';
   addCase.style.display = 'none';
-  btnGlass.classList.add("_active-btn-glass");
-  btnCase.classList.remove("_active-btn-case");
-  btnCharge.classList.remove("_active-btn-charge");
+  btnGlass.classList.add("_active-btn");
+  btnCase.classList.remove("_active-btn");
+  btnCharge.classList.remove("_active-btn");
+
 
 }
