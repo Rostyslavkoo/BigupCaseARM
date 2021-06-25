c80aa3d6ce2bd0f28a1598e81ad8454ec2359f89
Rostyk <65834087+Rostyslavkoo@users.noreply.github.com>
2021-06-25 14:08:42 +0300

    beautify
    beautify all files

--- assets/js/add-goods-modal.js
+++ assets/js/add-goods-modal.js
@@ -7,32 +7,37 @@ let btnGlass = document.querySelector('#btnAddGlass')
 let addGoodsModal = document.querySelector('#addGoodsModal')
 
 
-function show_cases(){
-  addCharge.style.display = 'none';
-  addGlass.style.display = 'none';
-  addCase.style.display = 'flex';
-  btnCase.classList.add("_active-btn");
-  btnGlass.classList.remove("_active-btn");
-  btnCharge.classList.remove("_active-btn");
-
-}
-function show_charge(){
-  addCharge.style.display = 'flex';
-  addGlass.style.display = 'none';
-  addCase.style.display = 'none';
-  btnCharge.classList.add("_active-btn");
-  btnCase.classList.remove("_active-btn");
-  btnGlass.classList.remove("_active-btn");
+function show_cases() {
+    addCharge.style.display = 'none';
+    addGlass.style.display = 'none';
+    addCase.style.display = 'flex';
+    btnCase.classList.add("_active-btn");
+    btnGlass.classList.remove("_active-btn");
+    btnCharge.classList.remove("_active-btn");
 
 
 }
-function show_glasse(){
-  addCharge.style.display = 'none';
-  addGlass.style.display = 'flex';
-  addCase.style.display = 'none';
-  btnGlass.classList.add("_active-btn");
-  btnCase.classList.remove("_active-btn");
-  btnCharge.classList.remove("_active-btn");
+
+function show_charge() {
+    addCharge.style.display = 'flex';
+    addGlass.style.display = 'none';
+    addCase.style.display = 'none';
+    btnCharge.classList.add("_active-btn");
+    btnCase.classList.remove("_active-btn");
+    btnGlass.classList.remove("_active-btn");
+
+
+
+}
+
+function show_glasse() {
+    addCharge.style.display = 'none';
+    addGlass.style.display = 'flex';
+    addCase.style.display = 'none';
+    btnGlass.classList.add("_active-btn");
+    btnCase.classList.remove("_active-btn");
+    btnCharge.classList.remove("_active-btn");
+
 
 
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,3 @@
 @@include('tooltip.js')
 @@include('input-quantity.js')
 @@include('add-goods-modal.js')
-@@include('input-text.js')
